5fc539a7da76fffa447bd51002a6fa5f43c53694
San-Pédro ADJOH <sanpedrox360@gmail.com>
2026-02-23 15:44:48 +0000

    Mise à jour

--- suite_de_Fibonacci.py
+++ suite_de_Fibonacci.py
@@ -1,16 +1,22 @@
 N=int(input("Entrez le nombre de termes de la suite de Fibonacci: "))
-sub=0
+somme=0
 a=0
 b=1
-if N<=0:
+while N<=0:
+ 
     print("Erreur: le nombre de termes doit être supérieur à zéro.")
-elif N==1:
+    N=int(input("Entrez le nombre de termes de la suite de Fibonacci: "))
+
+if N==1:
     print(a)
-else:
+elif N==2:
+    print(a)
+    print(b)
+else :
     print(a)
     print(b)
     for i in range (N-2):
-        sub=a+b
-        print(sub)
+        somme=a+b
+        print(somme)
         a=b
-        b=sub
+        b=somme
