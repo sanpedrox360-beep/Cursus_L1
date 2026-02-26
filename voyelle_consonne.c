ee0c4351ccdf63c46c7ad837e3ea9aeb1caebbd7
San-Pédro ADJOH <sanpedrox360@gmail.com>
2026-02-26 21:02:58 +0000

    1st commit

---
+++ voyelle_consonne.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    char ch;
+    printf("Saisir la lettre : ");
+    scanf("%c", &ch);
+    if ((ch == 'a') || (ch == 'e') || (ch == 'u') || (ch == 'i') || (ch == 'o') || (ch == 'y'))
+    {
+        printf("%c est une voyelle\n", ch);
+    }
+    else if ((ch >= 'a' && ch <= 'z') && !(ch == 'a' || ch == 'e' || ch == 'u' || ch == 'i' || ch == 'o' || ch == 'y'))
+    {
+
+        printf("%c est une consonne ", ch);
+    }
+    else
+    {
+        printf("Erreur ");
+    }
+
+    return 0;
+}
